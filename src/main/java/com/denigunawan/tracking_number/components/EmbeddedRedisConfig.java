28b4dbfbcb2d7cc359fc624bf074ae9930420c68
Deni Gunawan <denigunawan775@gmail.com>
2025-04-16 11:05:45 +0700

    add redis embbeded and h2 database

---
+++ src/main/java/com/denigunawan/tracking_number/components/EmbeddedRedisConfig.java
@@ -0,0 +1,26 @@
+package com.denigunawan.tracking_number.components;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        redisServer = new RedisServer(6379);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+}
